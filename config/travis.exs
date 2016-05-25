a4662aebf09cef94d3dfa898f8d751b5d13d5eff
Dave Shah <david.a.shah@gmail.com>
2016-05-25 11:41:38 -0400

    Travis

--- config/travis.exs
+++ config/travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :the_running_club, TheRunningClub.Endpoint,
+config :local_corral, LocalCorral.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,10 +10,10 @@ config :the_running_club, TheRunningClub.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :the_running_club, TheRunningClub.Repo,
+config :local_corral, LocalCorral.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
-  database: "the_running_club_test",
+  database: "local_corral_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
