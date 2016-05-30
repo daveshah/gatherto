4b873d9aae309b487694282fcb35b2798195dfbb
Dave Shah <david.a.shah@gmail.com>
2016-05-29 20:34:14 -0400

    Prep for users

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -8,4 +8,11 @@ defmodule Rnnr.AuthController do
     render(conn, "request.html", callback_url: Helpers.callback_url(conn))
   end
 
+  def identity_callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    case User.find_or_create(auth.credentials) do
+      :ok -> conn
+      { :error, reason } -> conn
+    end
+  end
+
 end
