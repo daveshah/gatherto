6ed5376f5665c6e110a6e192cf1f0e6008434454
Dave Shah <david.a.shah@gmail.com>
2016-07-12 14:01:22 -0400

    correct relationship

--- web/models/athlete_run.ex
+++ web/models/athlete_run.ex
@@ -2,7 +2,7 @@ defmodule Gatherto.AthleteRun do
   use Ecto.Schema
 
   schema "athletes_runs" do
-    belongs_to :athlete, Gatherto.AthleteRun
+    belongs_to :athlete, Gatherto.Athlete
     belongs_to :run, Gatherto.Run
     timestamps()
   end
