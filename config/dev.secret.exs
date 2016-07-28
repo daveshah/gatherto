cc4fc7bd3dca050fb2a4739d72bafa528b0c1bb6
Dave Shah <david.a.shah@gmail.com>
2016-07-28 19:17:27 -0400

    fixed untracked files

--- config/dev.secret.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :gatherto, Gatherto.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "phoenix",
-  password: "phoenix",
-  database: "gatherto_dev",
-  hostname: "localhost",
-  pool_size: 10
--- config/test.secret.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :gatherto, Gatherto.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "phoenix",
-  password: "phoenix",
-  database: "gatherto_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
