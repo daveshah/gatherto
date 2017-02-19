3769100e780f5843d23cd212724e21ef77ae8a66
Dave Shah <david.a.shah@gmail.com>
2017-02-19 09:40:07 -0500

    Demo WIP

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,7 +1,7 @@
 defmodule Gatherto.Messages do
 
   def run_message(run, athlete_numbers) do
-    msg = "Run on #{run.time} at #{run.location}"
+    msg = "Run posted for #{run.time} at #{run.location}. Details/RSVP's are here: https://gatherto.herokuapp.com/runs/#{run.id}"
     athlete_numbers |> Enum.each(&send_message(&1, msg))
   end
 
